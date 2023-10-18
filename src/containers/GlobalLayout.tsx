3f0f8f41b22dcafb987e75665cc5c6b8f104722c
udaceue <rjemik9@gmail.com>
2023-10-18 12:14:40 +0200

    added basic layout file, working on folder structure

---
+++ src/containers/GlobalLayout.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navigation from "@/components/Navigation.tsx";
+import MaxWidthWrapper from "@/containers/MaxWidthWrapper.tsx";
+
+function GlobalLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className={"max-w-screen overflow-x-hidden"}>
+            <Navigation />
+            <MaxWidthWrapper>
+          {children}
+            </MaxWidthWrapper>
+        </div>
+    );
+}
+
+export default GlobalLayout;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,19 @@
+import GlobalLayout from "@/containers/GlobalLayout.tsx";
 
 
 function Home() {
     return (
-        <div>Home</div>
+        <GlobalLayout>
+       <div className={"bg-red-500 grid md:grid-cols-[200px_minmax(500px,_1fr)]"}>
+           <div className={"hidden bg-yellow-200 p-2 md:flex"}>
+               Leagues Will Be Here asssssssssssssssssss
+           </div>
+           <div className={"bg-green-600 p-2"}>
+               Matches Will go here
+           </div>
+           {/*<div className={"bg-green-800"}>Xd2</div>*/}
+       </div>
+        </GlobalLayout>
     );
 }
 
