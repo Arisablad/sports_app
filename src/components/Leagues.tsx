dbc9ad5590d58d86f96836c77e28348d9d7f4726
udaceue <rjemik9@gmail.com>
2023-10-20 14:50:17 +0200

    added filter by country name

--- src/components/Leagues.tsx
+++ src/components/Leagues.tsx
@@ -1,9 +1,10 @@
 import LeaguesService from "@/services/LeaguesService.tsx";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { AxiosError } from "axios";
 import { Skeleton } from "@/components/ui/skeleton.tsx";
 import { ArrowDown, ArrowUp } from "lucide-react";
 import { TLeagues } from "@/types/LeaguesTypes.ts";
+import { Input } from "@/components/ui/input.tsx";
 
 function Leagues() {
   const { getAllLeagues } = LeaguesService();
@@ -12,6 +13,18 @@ function Leagues() {
     "https://static.vecteezy.com/system/resources/previews/002/486/569/non_2x/404-error-icon-vector.jpg";
 
   const [expandedLeagues, setExpandedLeagues] = useState([] as string[]);
+  const [inputValue, setInputValue] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const filteredLeagues = useMemo(() => {
+    if (inputValue === "") {
+      return leagues;
+    } else {
+      return leagues.filter((league) =>
+        league.Cnm.toLowerCase().includes(inputValue.toLowerCase()),
+      );
+    }
+  }, [inputValue, leagues]);
 
   const ExpandLeagues = (id: string) => {
     setExpandedLeagues((prev): string[] | [] => {
@@ -24,6 +37,7 @@ function Leagues() {
   };
 
   useEffect(() => {
+    setLoading(true);
     getAllLeagues()
       .then((response: any) => {
         console.log(response);
@@ -33,87 +47,107 @@ function Leagues() {
       })
       .catch((error: AxiosError) => {
         console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
       });
-  });
+  }, []);
 
   return (
     <div
       className={"hidden w-full h-full min-h-[700px] md:flex flex-col gap-2"}
     >
-      {leagues && leagues.length > 0
-        ? leagues.map((league) => (
+      {/*SearchInput*/}
+      <div className="grid w-full max-w-sm py-2">
+        <Input
+          type="text"
+          id="SearchLeague"
+          className={"text-black"}
+          placeholder="Search by country"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+      </div>
+
+      {/*Leagues*/}
+      {filteredLeagues && filteredLeagues.length > 0 ? (
+        filteredLeagues.map((league) => (
+          <div
+            className={" flex flex-col gap-2 rounded-lg cursor-pointer"}
+            key={league.Cid}
+          >
             <div
-              className={" flex flex-col gap-2 rounded-lg cursor-pointer"}
-              key={league.Cid}
+              key={league.Cnm}
+              className={
+                "bg-[#111827] text-white rounded-sm py-1.5 px-4 font-sans font-medium flex items-center gap-4 hover:bg-[#333333]/60 transition duration-300"
+              }
+              onClick={() => ExpandLeagues(league.Cid)}
             >
-              <div
-                key={league.Cnm}
-                className={
-                  "bg-[#111827] text-white rounded-sm py-1.5 px-4 font-sans font-medium flex items-center gap-4 hover:bg-[#333333]/60 transition duration-300"
-                }
-                onClick={() => ExpandLeagues(league.Cid)}
-              >
-                <img
-                  src={`https://static.livescore.com/i2/fh/${league.Ccd}.jpg`}
-                  onError={(e) => {
-                    (e.target as any).onError = null;
-                    (e.target as HTMLImageElement).src = fallbackSrc;
-                  }}
-                  loading="lazy"
-                  className={"h-4 w-6"}
-                  alt={league.Cnm}
-                />
-                <div className={"flex items-center w-full justify-between"}>
-                  <span>{league.Cnm}</span>
-                  {expandedLeagues.includes(league.Cid) ? (
-                    <span>
-                      <ArrowUp />
-                    </span>
-                  ) : (
-                    <span>
-                      <ArrowDown />
-                    </span>
-                  )}
-                </div>
+              <img
+                src={`https://static.livescore.com/i2/fh/${league.Ccd}.jpg`}
+                onError={(e) => {
+                  (e.target as any).onError = null;
+                  (e.target as HTMLImageElement).src = fallbackSrc;
+                }}
+                loading="lazy"
+                className={"h-4 w-6"}
+                alt={league.Cnm}
+              />
+              <div className={"flex items-center w-full justify-between"}>
+                <span>{league.Cnm}</span>
+                {expandedLeagues.includes(league.Cid) ? (
+                  <span>
+                    <ArrowUp />
+                  </span>
+                ) : (
+                  <span>
+                    <ArrowDown />
+                  </span>
+                )}
               </div>
-              {/*Dropdown*/}
-              {expandedLeagues.includes(league.Cid)
-                ? league.Stages?.map((stage) => (
+            </div>
+            {/*Dropdown*/}
+            {expandedLeagues.includes(league.Cid)
+              ? league.Stages?.map((stage) => (
+                  <div
+                    className={
+                      "border-gray-600 flex flex-col gap-2 rounded-lg cursor-pointer"
+                    }
+                  >
+                    {/*Dropdown content*/}
+
                     <div
+                      key={league.Cnm}
                       className={
-                        "border-gray-600 flex flex-col gap-2 rounded-lg cursor-pointer"
+                        "bg-[#333333] text-white rounded-sm py-1.5 px-4 font-sans font-medium flex items-center gap-4 hover:bg-[#333333]/60 transition duration-300"
                       }
                     >
-                      {/*Dropdown content*/}
-
-                      <div
-                        key={league.Cnm}
-                        className={
-                          "bg-[#333333] text-white rounded-sm py-1.5 px-4 font-sans font-medium flex items-center gap-4 hover:bg-[#333333]/60 transition duration-300"
-                        }
-                      >
-                        <img
-                          src={`https://static.livescore.com/i2/fh/${league.Ccd}.jpg`}
-                          onError={(e) => {
-                            (e.target as any).onError = null;
-                            (e.target as HTMLImageElement).src = fallbackSrc;
-                          }}
-                          loading="lazy"
-                          className={"h-4 w-6"}
-                          alt={league.Cnm}
-                        />
-                        <div className={"flex items-center w-14"}>
-                          {stage.Sdn}
-                        </div>
+                      <img
+                        src={`https://static.livescore.com/i2/fh/${league.Ccd}.jpg`}
+                        onError={(e) => {
+                          (e.target as any).onError = null;
+                          (e.target as HTMLImageElement).src = fallbackSrc;
+                        }}
+                        loading="lazy"
+                        className={"h-4 w-6"}
+                        alt={league.Cnm}
+                      />
+                      <div className={"flex items-center w-14"}>
+                        {stage.Sdn}
                       </div>
                     </div>
-                  ))
-                : null}
-            </div>
-          ))
-        : Array.from({ length: 20 }).map((_, i) => (
-            <Skeleton key={i} className={"w-full h-8"} />
-          ))}
+                  </div>
+                ))
+              : null}
+          </div>
+        ))
+      ) : loading ? (
+        Array.from({ length: 20 }).map((_, i) => (
+          <Skeleton key={i} className={"w-full h-8"} />
+        ))
+      ) : (
+        <p className={"text-center"}>No Countries found</p>
+      )}
     </div>
   );
 }
